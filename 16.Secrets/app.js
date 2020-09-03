761730f9c51d3e6aadcfc9dbd0bab6bf9c0231e6
Anil Desireddy <anil.dr197@gmail.com>
2020-09-03 19:12:46 +0530

    lev 6 security for secrets with passport-google-oauth(cool and complicated stuff)

--- 16.Secrets/app.js
+++ 16.Secrets/app.js
@@ -7,6 +7,8 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");  //passport local isn't required by us it is a dependancy for passportLocalMongoose
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate");//to support find or create in the google strategy callback function
 //const encrypt = require("mongoose-encryption");      //for encrypting fields
 //const md5 = require("md5");             //a relatively easy to hack hash function
 //const bcrypt = require("bcrypt");         //industry standard hash function along with built in salt capabilities
@@ -35,24 +37,61 @@ mongoose.set('useCreateIndex', true); //dealing with a deprecation warning
 
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  googleId: String,
+  secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);   //used to hash and salt passwords and save users into database
+userSchema.plugin(findOrCreate);
 //userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = mongoose.model("User", userSchema);
 
 //by using passportLocalMongoose instead of directly dealing with passportLocal only the below three lines of code is needed rest is taken care by passportLocalMongoose
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+//passport.serializeUser(User.serializeUser());
+//passport.deserializeUser(User.deserializeUser());
+//above lines worked for local authentication, to enable google authentication below piece of code is taken from passport documentation config
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
 
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
 
+//it is important to follow the sequence of process through out the setup //general tip
+//the following  piece if directly form passport-google-oauth20 documentation
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'//this is to deal with google+ deprication as passport-google-oauth20 has it as a dependency
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/",function(req,res){
   res.render("home");
 });
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['profile'] })); //takes user to google server to authenticate
+
+app.get('/auth/google/secrets',
+  passport.authenticate('google', { failureRedirect: '/login' }), //authenticates locally and save login session
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
 
 app.get("/login",function(req,res){
   res.render("login");
@@ -63,8 +102,20 @@ app.get("/register",function(req,res){
 });
 
 app.get("/secrets",function(req, res){
+  User.find({"secret": {$ne: null}}, function(err, result){
+    if(err)
+      console.log(err);
+    else {
+      if(result){
+        res.render("secrets", {usersWithSecrets: result});
+      }
+    }
+  });
+});
+
+app.get("/submit", function(req, res){
   if(req.isAuthenticated()){
-    res.render("secrets");
+    res.render("submit");
   }
   else{
     res.redirect("/login");
@@ -110,7 +161,19 @@ app.post("/login", function(req, res){
   });
 });
 
-
+app.post("/submit", function(req, res){
+  const submittedSecret = req.body.secret;
+  User.findById(req.user.id, function(err, result){ //current user is stored in session by passport and can be accesed by req.user
+    if(err)
+      console.log(err);
+    else {
+      result.secret = submittedSecret;
+      result.save(function(){
+        res.redirect("/secrets");
+      });
+    }
+  });
+});
 
 app.listen(3000, function(){
   console.log("server up on 3000");
